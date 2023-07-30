0736ad3358707ac2bf92988700ff7ab2db643ace
Lion999CAP <llaras1@est.ups.edu.ec>
2023-07-30 11:14:08 -0500

    Commit 1.1 subida de archivos

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { GastoService } from './gasto.service';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { InformacionComponent } from './informacion/informacion.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ReporteComponent } from './reporte/reporte.component';
+
+const rutas: Routes = [
+  { path: 'informacion', component: InformacionComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'menu', component: MenuComponent},
+  { path: 'reporte', component: ReporteComponent }
+  ];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    InformacionComponent,
+    FormularioComponent,
+    ReporteComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(rutas)
+  ],
+  providers: [GastoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
